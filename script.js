d35fd06e84aff9e8cb802ea5d20457297c3f1651
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-06-19 18:23:22 +0100

    five stars added, enrollment logic fixed, contact page styling fixed, profile styling improved

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const developersData = [
     available: "Yes",
     pricePerHour: 100,
     image: "./images/AliceJames.png",
-    bio: "Hi there, I'm Alice James, an experienced JavaScript developer with a passion for creating dynamic and responsive web applications. With over a decade of experience in the industry, I've had the opportunity to work on a wide range of projects, from small startups to large-scale enterprise applications. My expertise includes front-end development using modern frameworks like React, Vue, and Angular, as well as server-side programming with Node.js. I'm dedicated to writing clean, efficient code and staying updated with the latest trends in web development.",
+    bio: "An experienced JavaScript developer with a passion for creating dynamic and responsive web applications. With over a decade of experience in the industry, I've had the opportunity to work on a wide range of projects, from small startups to large-scale enterprise applications. My expertise includes front-end development using modern frameworks like React, Vue, and Angular, as well as server-side programming with Node.js. I'm dedicated to writing clean, efficient code and staying updated with the latest trends in web development.",
     deliveryTime: 1
   },
   {
@@ -19,7 +19,7 @@ const developersData = [
     available: "No",
     pricePerHour: 60,
     image: "./images/JamesRoedan.png",
-    bio: "Hello, I'm James Roedan, a Python enthusiast specializing in backend development and data science. With a master's degree in computer science and years of industry experience, I've become proficient in designing and implementing scalable, high-performance systems. My expertise extends to machine learning and artificial intelligence, where I've developed predictive models and recommendation systems for various applications. I'm passionate about solving complex problems and contributing to projects that make a positive impact.",
+    bio: "A Python enthusiast specialising in backend development and data science. With a master's degree in computer science and years of industry experience, I've become proficient in designing and implementing scalable, high-performance systems. My expertise extends to machine learning and artificial intelligence, where I've developed predictive models and recommendation systems for various applications. I'm passionate about solving complex problems and contributing to projects that make a positive impact.",
     deliveryTime: 1
   },
   {
@@ -30,7 +30,7 @@ const developersData = [
     available: "Yes",
     pricePerHour: 55,
     image: "./images/CharlieWano.png",
-    bio: "I'm Charlie Wano, a seasoned Java developer with a focus on enterprise-level applications. Over the past fifteen years, I've worked with leading companies to design and develop robust software solutions that meet their business needs. My expertise spans the entire software development lifecycle, from requirements analysis and architecture design to implementation and maintenance. I have a strong background in backend development using Java EE, Spring Framework, and Hibernate, and I'm committed to delivering high-quality code that meets both functional and non-functional requirements.",
+    bio: "A Java developer with a focus on enterprise-level applications. Over the past fifteen years, I've worked with leading companies to design and develop robust software solutions that meet their business needs. My expertise spans the entire software development lifecycle, from requirements analysis and architecture design to implementation and maintenance. I have a strong background in backend development using Java EE, Spring Framework, and Hibernate, and I'm committed to delivering high-quality code that meets both functional and non-functional requirements.",
     deliveryTime: 24
   },
   {
@@ -41,7 +41,7 @@ const developersData = [
     available: "Yes",
     pricePerHour: 45,
     image: "./images/ChristinaRoof.png",
-    bio: "Greetings! I'm Christina Roof, a Ruby developer with a passion for building elegant and maintainable software solutions. Over the past eight years, I've worked with startups and established companies to develop web applications that are not only user-friendly but also scalable and secure. My expertise includes Ruby on Rails, Sinatra, and other Ruby frameworks, as well as front-end technologies like HTML, CSS, and JavaScript. I believe in writing clean, readable code and following best practices to ensure the long-term maintainability of projects.",
+    bio: "A Ruby developer with a passion for building elegant and maintainable software solutions. Over the past eight years, I've worked with startups and established companies to develop web applications that are not only user-friendly but also scalable and secure. My expertise includes Ruby on Rails, Sinatra, and other Ruby frameworks, as well as front-end technologies like HTML, CSS, and JavaScript. I believe in writing clean, readable code and following best practices to ensure the long-term maintainability of projects.",
     deliveryTime: 48
   },
   {
@@ -52,7 +52,7 @@ const developersData = [
     available: "No",
     pricePerHour: 65,
     image: "./images/XaviGonzalez.png",
-    bio: "Hello, I'm Xavi Gonzalez, a dedicated C# developer specializing in Windows application development and .NET technologies. With a background in computer engineering and several years of industry experience, I've developed a deep understanding of the Microsoft ecosystem. I've worked on a wide range of projects, from desktop applications to web services, using technologies like WPF, ASP.NET Core, and Xamarin. I'm passionate about crafting high-quality software solutions that leverage the full power of the .NET platform.",
+    bio: "A dedicated C# developer specialising in Windows application development and .NET technologies. With a background in computer engineering and several years of industry experience, I've developed a deep understanding of the Microsoft ecosystem. I've worked on a wide range of projects, from desktop applications to web services, using technologies like WPF, ASP.NET Core, and Xamarin. I'm passionate about crafting high-quality software solutions that leverage the full power of the .NET platform.",
     deliveryTime: 48
   },
   {
@@ -63,7 +63,7 @@ const developersData = [
     available: "Yes",
     pricePerHour: 40,
     image: "./images/FrankJackson.png",
-    bio: "I am Frank Jackson, an experienced Python developer with a strong foundation in backend web development. Over the past ten years, I've built custom web applications for clients across various industries, from e-commerce platforms to content management systems.",
+    bio: "An experienced Python developer with a strong foundation in backend web development. Over the past ten years, I've built custom web applications for clients across various industries, from e-commerce platforms to content management systems.",
     deliveryTime: 1
   },
   {
@@ -74,7 +74,7 @@ const developersData = [
     available: "Yes",
     pricePerHour: 55,
     image: "./images/HenrichBurchards.png",
-    bio: "Hey there! I'm Henrich Burchards, a Ruby developer passionate about crafting delightful pplications. With a background in mobile app development and a keen eye for design, I've had the privilege of working on numerous projects, ranging from consumer apps to enterprise solutions.",
+    bio: "A Ruby developer passionate about crafting delightful pplications. With a background in mobile app development and a keen eye for design, I've had the privilege of working on numerous projects, ranging from consumer apps to enterprise solutions.",
     deliveryTime: 8
   },
 ];
@@ -98,7 +98,7 @@ function renderDevelopers(developers) {
           showProfile(developer);
         });
 
-        const shortenedBio = developer.bio.substring(0, 250) + "...";
+        const shortenedBio = developer.bio.substring(0, 150) + "...";
 
 
         developerDiv.innerHTML = `
